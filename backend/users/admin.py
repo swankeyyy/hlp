9ee9803e8b8c5d1fab57b35a0dbedd941843e5a6
levchuk <swankyyy1@gmail.com>
2024-05-02 12:49:24 +0300

    create user model with user.image, user.phone
    add model to admin
    set media urls and collect static

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "first_name", "last_name")
+    list_filter = ("username",)
+
+    save_on_top = True
+    save_as = True
+
+
+
+admin.site.site_header = 'Админка helper'
+admin.site.index_title = 'Управление'
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to="users/images/", blank=True, null=True, verbose_name="Фото профиля")
+    phone = models.IntegerField(verbose_name="Номер телефона", null=True, blank=True, default=0)
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
