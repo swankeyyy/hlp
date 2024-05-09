cdabd0fd79a167927d97b685cd51d9cbd3af2bda
levchuk <swankyyy1@gmail.com>
2024-05-09 21:01:58 +0300

    refactor base.html by components realize recursetree in header menu

--- backend/blog/templatetags/blog_tags.py
+++ backend/blog/templatetags/blog_tags.py
@@ -5,9 +5,9 @@ from blog.models import Category
 register = template.Library()
 
 
-@register.inclusion_tag("UI/tags/header/header_menu.html")
-def get_menu():
+@register.inclusion_tag("UI/tags/header/header_menu.html", takes_context=True)
+def get_header_menu(context):
     """all categories for top menu"""
-    content = Category.objects.all()
+    content = Category.objects.filter(mptt_level=0)
     return {"menu": content}
 
