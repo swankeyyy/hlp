c6c48785fdd8b7d9a135f6a355e8aab723372c1a
levchuk <swankyyy1@gmail.com>
2024-05-10 01:32:08 +0300

    new model IP add MainPageView for Main page

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.list import ListView
 from .models import Post
+from common.views import CommonTitleMixin
 
 # asdsad
-class AllPostsView(ListView):
+class AllPostsView(CommonTitleMixin, ListView):
+    title = "Главная"
     model = Post
     template_name = 'blog/all_posts.html'
     context_object_name = 'posts'
+    
+    
---
+++ backend/common/views.py
@@ -0,0 +1,7 @@
+class CommonTitleMixin:
+    title = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
