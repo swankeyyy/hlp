bbdd67ad89521ef19c5e0bc5c54578d88d1c4fca
levchuk <swankyyy1@gmail.com>
2024-05-10 10:50:44 +0300

    realized search at modal search winodow, it searching by tag and then by post name add error message to html if search is null

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,14 +1,47 @@
+from typing import Any
+from django.db.models import Q
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.list import ListView
 from .models import Post
 from common.views import CommonTitleMixin
 
+
 # asdsad
 class AllPostsView(CommonTitleMixin, ListView):
     title = "Главная"
     model = Post
-    template_name = 'blog/all_posts.html'
-    context_object_name = 'posts'
-    
-    
+    template_name = "blog/all_posts.html"
+    context_object_name = "posts"
+    error_search_message = None
+
+    def _get_content_by_search(self, instance, search):
+        """takes search value and instance and check:
+        if value in post name it returns list of posts
+        elif value == tag.name it returns all posts by tag name
+        elif result is null it returns empty queryset and error_message
+        """
+
+        if search:
+            queryset_by_tag = instance.filter(tag__name=search)
+            if queryset_by_tag:
+                return queryset_by_tag
+            queryset_by_icontains = instance.filter(name__icontains=search)
+            if queryset_by_icontains:
+                return queryset_by_icontains
+            instance = []
+            self.error_search_message = f"по запросу '{search}' результатов не найдено!..."
+
+        return instance
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset().filter(is_published=True)
+        search = self.request.GET.get("q")
+        content = self._get_content_by_search(queryset, search)
+        return content
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['error_search_message'] = self.error_search_message
+        return context
